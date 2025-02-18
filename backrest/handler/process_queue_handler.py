b5f8d27574c3256eb7196647bce36fb6a44fc3dd
Martijn Schot <martijn@schot.co.uk>
2025-02-18 20:42:15 +0000

    Adds removal of backup directory if backup fails

--- backrest/handler/process_queue_handler.py
+++ backrest/handler/process_queue_handler.py
@@ -1,9 +1,13 @@
 
+import shutil
+from pathlib import Path
 from queue import Queue
 
+from backrest.config.config_factory import config_factory
 from backrest.data.adapter.adapter_factory import (
     adapter_factory as data_adapter_factory,
 )
+from backrest.data.model.process import Process
 from backrest.data.transformer.process_to_backup import process_to_backup
 from backrest.logger.logger_factory import logger_factory
 
@@ -12,6 +16,7 @@ class ProcessQueueHandler:
     def __init__(self, queue: Queue) -> None:
         self.queue = queue
         self.logger = logger_factory()
+        self.config = config_factory()
 
     def handle(self) -> None:
         while True:
@@ -24,8 +29,7 @@ class ProcessQueueHandler:
                     process.pid, process.return_code,
                 )
 
-                #@TODO do cleanup of backup folder in case it was created but
-                # not completed the process has the identifier in the args for backups
+                self.cleanup(process)
                 continue
 
             data_adapter = data_adapter_factory()
@@ -36,3 +40,29 @@ class ProcessQueueHandler:
             elif process.type == "restore":
                 self.logger.debug("Restore completed successfully")
             break
+
+    def cleanup(self, process: Process) -> None:
+
+        if (
+            process.type == "backup" and hasattr(process, "args")
+            and process.args.get("identifier")
+        ):
+            self.remove_backup_folder(process.args.get("identifier"))
+
+    def remove_backup_folder(self, identifier: str) -> None:
+        # do cleanup of backup folder in case it was created but not completed
+        backup_path = Path(
+            f"{self.config.value("backup_dir").rstrip("/")}/{identifier}",
+        )
+        if backup_path.exists():
+            try:
+                shutil.rmtree(backup_path)
+                self.logger.debug(
+                    "Cleaned up incomplete backup folder: %s",
+                    backup_path,
+                )
+            except Exception:
+                self.logger.exception(
+                    "Failed to cleanup backup folder %s",
+                    backup_path,
+                )
