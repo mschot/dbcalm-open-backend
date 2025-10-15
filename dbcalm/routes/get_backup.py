c42ce24dc244cb34e9055f30e5700556ed5109fd
Martijn Schot <martijnschotnl@gmail.com>
2025-10-15 18:33:04 +0100

    Removes build files, updates git ignore adds missing route file

---
+++ dbcalm/routes/get_backup.py
@@ -0,0 +1,22 @@
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, HTTPException
+
+from dbcalm.auth.verify_token import verify_token
+from dbcalm.data.repository.backup import BackupRepository
+
+router = APIRouter()
+
+
+@router.get("/backups/{backup_id}")
+async def get_backup(
+    backup_id: str,
+    _: Annotated[dict, Depends(verify_token)],
+) -> dict:
+    backup_repo = BackupRepository()
+    backup = backup_repo.get(backup_id)
+
+    if not backup:
+        raise HTTPException(status_code=404, detail="Backup not found")
+
+    return backup.model_dump()
